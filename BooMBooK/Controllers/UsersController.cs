631432599850a33214ab316d4b47b9c3b93da5b1
BohdanPaniv <bohdan.paniv@gmail.com>
2021-03-21 17:10:55 +0200

    Add Controller

---
+++ BooMBooK/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using BooMBooK.Models.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BooMBooK.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        static readonly List<User> users;
+        static UsersController()
+        {
+            users = new List<User>();
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Post(User user)
+        {
+            user.UserId = Guid.NewGuid().ToString();
+            users.Add(user);
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            User user = users.FirstOrDefault(x => x.UserId == id);
+            
+            if (user == null)
+            {
+                return NotFound();
+            }
+            users.Remove(user);
+            return Ok(user);
+        }
+    }
+}
