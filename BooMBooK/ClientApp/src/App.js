5c084d45fc5705ae078cb67297d5a045938ee243
den oshipok <oshipok.denis@gmail.com>
2021-04-14 21:48:50 +0300

    Create profile page

--- BooMBooK/ClientApp/src/App.js
+++ BooMBooK/ClientApp/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import {UserSetting} from "./components/UserSetting/UserSetting"
+import { UserSetting } from "./components/UserSetting/UserSetting"
+import { Profile } from "./components/Profile/Profile"
 // import { AuthenticationPage } from "./components/AuthenticationPage"
 
 import './custom.css'
@@ -16,7 +17,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route exact path='/Settings' component={UserSetting} />
         {/*<Route exact path='/Authentication' component={AuthenticationPage}/>*/}
-        {/*<Route exact path='/Profile' component={Profile}/>*/}
+        <Route exact path='/Profile' component={Profile}/>
       {/*  <Route path='/counter' component={Counter} />*/}
       {/*  <Route path='/fetch-data' component={FetchData} />*/}
       </Layout>
