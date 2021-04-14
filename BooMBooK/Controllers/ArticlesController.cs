99e7fc443f03718ef54f819b9ff41fb5e0051971
BohdanPaniv <bohdan.paniv@gmail.com>
2021-04-14 21:58:57 +0300

    Fiz

--- BooMBooK/Controllers/ArticlesController.cs
+++ BooMBooK/Controllers/ArticlesController.cs
@@ -40,7 +40,6 @@ namespace BooMBooK.Controllers
         {
             int first = Convert.ToInt32(firstNumber);
             int second = Convert.ToInt32(secondNumber);
-            var temp = await articleService.GetArticles(first, second);
             return await articleService.GetArticles(first, second);
         }
     }
