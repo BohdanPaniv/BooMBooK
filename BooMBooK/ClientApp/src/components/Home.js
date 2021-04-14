c8f70d602d4f3d826fdc86b989cab836d7fb54e6
Taras Pylypchyn <rever.forever1@gmail.com>
2021-04-14 23:07:55 +0300

    Change getList method in Home.js.

--- BooMBooK/ClientApp/src/components/Home.js
+++ BooMBooK/ClientApp/src/components/Home.js
@@ -9,55 +9,59 @@ import "./Home.css";
 // };
 
 export function Home() {
+
+
     const [articleList,setArticleList] = useState([]);
 
-    function getArticles(){
-        let xhr = new XMLHttpRequest();
 
-        xhr.open("get","api/articles/0,12", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                setArticleList(JSON.parse(xhr.responseText));
-            }
-        };
-        xhr.send();
-        console.log(xhr);
-    }
-
-    let val = 0;
-
-    function setArticle() {
-        let xhr = new XMLHttpRequest();
-        let date =  new Date();
-
-        let news = JSON.stringify({
-            ArticleId:"",
-            UserId: "d12s",
-            DateTime: date,
-            Body_Article: "<div>Текст статі: "+val+"</div>",
-            Status: false,
-            Title: "Заголовок статі" + val
-        });
-
-        xhr.open("post","api/articles/", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                console.log(xhr.responseText);
-                val = val + 1;
-            }
-        };
-        xhr.send(news);
-        console.log(xhr);
-    }
     useEffect(()=>{
-       if (articleList.length === 0) getArticles();
+        function getArticles(){
+            let xhr = new XMLHttpRequest();
 
-       console.log(1)
-    })
+            xhr.open("get","api/articles/0,12", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    setArticleList(JSON.parse(xhr.responseText));
+                }
+            };
+            xhr.send();
+            console.log(xhr);
+        }
+        if (articleList.length === 0) getArticles();
+    },[articleList])
+
+
+
+    // let val = 0;
+
+    // function setArticle() {
+    //     let xhr = new XMLHttpRequest();
+    //     let date =  new Date();
+    //
+    //     let news = JSON.stringify({
+    //         ArticleId:"",
+    //         UserId: "d12s",
+    //         DateTime: date,
+    //         Body_Article: "<div>Текст статі: "+val+"</div>",
+    //         Status: false,
+    //         Title: "Заголовок статі" + val
+    //     });
+    //
+    //     xhr.open("post","api/articles/", true);
+    //     xhr.setRequestHeader("Content-Type", "application/json");
+    //
+    //     xhr.onload = function () {
+    //         if (xhr.status === 200) {
+    //             console.log(xhr.responseText);
+    //             val = val + 1;
+    //         }
+    //     };
+    //     xhr.send(news);
+    //     console.log(xhr);
+    // }
+
     // window.onload = getArticles();
 
     return (
