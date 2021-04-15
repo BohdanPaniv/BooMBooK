454bd088a3b6e384e85d2319593a56038d72dfb8
den oshipok <oshipok.denis@gmail.com>
2021-04-15 21:48:53 +0300

    Add user articles show

--- BooMBooK/ClientApp/src/components/Profile/Profile.js
+++ BooMBooK/ClientApp/src/components/Profile/Profile.js
@@ -15,9 +15,8 @@ export function Profile() {
     function getArticles() {
         let xhr = new XMLHttpRequest();
 
-
-
-        xhr.open("get", "api/articles/0,12", true);
+        console.log(user);
+        xhr.open("get", "api/articles/userId/" + user.userId, true);
         xhr.setRequestHeader("Content-Type", "application/json");
 
         xhr.onload = function () {
@@ -29,36 +28,9 @@ export function Profile() {
         console.log(xhr);
     }
 
-    // let val = 0;
-
-    // function setArticle() {
-    //     let xhr = new XMLHttpRequest();
-    //     let date = new Date();
-    //
-    //     let news = JSON.stringify({
-    //         ArticleId: "",
-    //         UserId: "d12s",
-    //         DateTime: date,
-    //         Body_Article: "<div>Текст статі: " + val + "</div>",
-    //         Status: false,
-    //         Title: "Заголовок статі" + val
-    //     });
-    //
-    //     xhr.open("post", "api/articles/", true);
-    //     xhr.setRequestHeader("Content-Type", "application/json");
-    //
-    //     xhr.onload = function () {
-    //         if (xhr.status === 200) {
-    //             console.log(xhr.responseText);
-    //             val = val + 1;
-    //         }
-    //     };
-    //     xhr.send(news);
-    //     console.log(xhr);
-    // }
 
     useEffect(() => {
-        if (articleList.length === 0) getArticles();
+        if (articleList.length === 0 && user) getArticles();
 
         console.log(1)
     })
@@ -74,6 +46,7 @@ export function Profile() {
                         <div className="names">{user.firstName + ' ' + user.lastName}</div>
                         <div className="your-articles">Your article`s</div>
                         <div className="articles-box">
+                            
                         </div>
                     </div>
                 )
